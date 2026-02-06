5857c5bce38a3615f652931e1aa4fb6beff77f0d
Apollo0893 <ryan.eisenhour@gmail.com>
2026-02-06 14:36:56 -0500

    Update bible.py

--- app/bible.py
+++ app/bible.py
@@ -1,13 +1,13 @@
 import requests
 
-def get_verses(topic, limit):
+def get_verses(topic, limit=12):
     verses = []
     page = 1
 
     while len(verses) < limit:
         r = requests.get(
             "https://bible-api.com/search",
-            params={"query": topic, "limit": 10, "page": page}
+            params={"query": topic, "limit": 12, "page": page}
         )
         data = r.json()
         verses.extend(v["text"] for v in data.get("verses", []))
