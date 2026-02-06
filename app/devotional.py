01454377af2d8b0860582edfea7d5c50a2bec434
Apollo0893 <ryan.eisenhour@gmail.com>
2026-02-06 14:35:21 -0500

    Update devotional.py

--- app/devotional.py
+++ app/devotional.py
@@ -2,6 +2,6 @@ from bible import get_verses
 from llm import generate_reflection
 
 def build_devotional(topic):
-    verses = get_verses(topic)
+    verses = get_verses(topic, 12)
     reflection = generate_reflection(topic, verses)
     return "\n".join(verses) + "\n\n" + reflection
