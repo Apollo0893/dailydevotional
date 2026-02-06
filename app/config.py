70798f185f834805d5816d51f647eae1b2bd625f
Apollo0893 <ryan.eisenhour@gmail.com>
2026-02-06 13:54:32 -0500

    Update config.py
    add devotional length option portainer envs

--- app/config.py
+++ app/config.py
@@ -11,3 +11,9 @@ INJECT_DIR = os.getenv("INJECT_DIR", "/inject")
 SONGS_DIR = os.getenv("SONGS_DIR", "/songs")
 SONGS_PER_DAY = int(os.getenv("SONGS_PER_DAY", 3))
 VOICE = os.getenv("VOICE", "en-US-AriaNeural")
+
+MIN_DEVOTIONAL_MINUTES = int(os.getenv("MIN_DEVOTIONAL_MINUTES", 15))
+WORDS_PER_MINUTE = int(os.getenv("WORDS_PER_MINUTE", 150))
+MIN_WORDS = MIN_DEVOTIONAL_MINUTES * WORDS_PER_MINUTE
+
+MIN_VERSES = int(os.getenv("MIN_VERSES", 10))
